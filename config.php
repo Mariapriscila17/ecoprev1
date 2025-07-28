bf316934024ec9fd319e2f57bfbd52bbc779438b
Isabelly Lacerda <santosisabelly771@gmail.com>
2025-07-28 10:13:25 -0300

    atualizacao

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 class BancoDeDados {
-    private $host = "127.0.0.1";
+    private $host = "localhost:49160";
     private $nome_banco = "cadastro";
     private $usuario = "root";
     private $senha = "";
